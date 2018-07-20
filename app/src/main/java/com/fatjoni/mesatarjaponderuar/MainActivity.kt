757400badba91a80ffa0125918d5672b4c199398
fatjanhushi <me@fatjoni.com>
2018-07-20 14:05:21 +0200

    first commit

---
+++ app/src/main/java/com/fatjoni/mesatarjaponderuar/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.fatjoni.mesatarjaponderuar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fatjoni.mesatarjaponderuar.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
